3118988b292b858fe48f4ab56d14271ea04def6f
Nithin <nithinkv@live.com>
2018-04-05 13:04:20 +0530

    Initial commit

---
+++ QtpTests/ValidUserCanSuccessfullyLogin3.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace QtpTests
+{
+    [TestClass]
+    public class ValidUserCanSuccessfullyLogin3
+    {
+        //IMPORTANT - ChromeDriver has been removed and replaced with FirefoxDriver for less compatibility issues
+        //Please use FirefoxDriver throughout your code to
+        static  IWebDriver driver = new FirefoxDriver();
+        
+        [TestMethod]
+        public void RunTest()
+        {
+            //here we create a new instance of the Chrome driver
+            GoTo();
+            Login("seleniumTestUser", "Test12345!!$");
+            var loggedInHeader = driver.FindElement(By.XPath("//h1[text()='My Membership']"));
+            Assert.IsTrue(loggedInHeader.Displayed, "The user was not able to successfully login.");
+        }
+
+        public void Login(string userName, string password)
+        {
+            //find the field for ther user name
+            SendKeys("user_login", userName);
+
+            //find the field for the password
+            SendKeys("user_pass", password);
+            ClickButton("wp-submit");
+        }
+
+        public void SendKeys(string id, string value)
+        {
+            var user = driver.FindElement(By.Id(id));
+            user.SendKeys(value);
+        }
+
+        public void ClickButton(string id)
+        {
+            driver.FindElement(By.Id(id)).Click();
+        }
+
+        public void GoTo()
+        {
+            driver.Navigate().GoToUrl("https://www.qtptutorial.net/wp-login.php");
+
+        }
+    }
+}
